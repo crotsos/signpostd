eded0624365a8128f2633165a99a38d343036c56
crotsos <cr409@cl.cam.ac.uk>
2012-10-13 00:46:40 +0000

    remote control app fixes

--- lib/remoteControl.ml
+++ lib/remoteControl.ml
@@ -20,24 +20,38 @@ open Printf
 open Int64
 open Re_str
 
-let bind_fd ~port =
-  let src = Unix.ADDR_INET (Unix.inet_addr_any, (to_int port)) in 
-  let fd = Lwt_unix.(socket PF_INET SOCK_DGRAM 0) in
-  let () = Lwt_unix.bind fd src in
-  return fd
-
+module IncomingSignalling = SignalHandler.Make (ClientSignalling)
 let usage () = 
-  return (Printf.printf "Invalid arg\n%!")
+  Printf.printf "Usage: %s tactic_name\n%!" Sys.argv.(0)
+
+let create_fd address port =
+  let fd = Lwt_unix.(socket PF_INET SOCK_STREAM 0) in
+    return fd
 
 lwt _ =
-  try 
-    let server_ip = Sys.argv.(1) in 
-    let remote_port = (int_of_string Sys.argv.(2)) in 
-    let command = Sys.argv.(3) in 
-    let fd = Lwt_unix.(socket PF_INET SOCK_DGRAM 0) in
-    let ipaddr = (Unix.gethostbyname server_ip).Unix.h_addr_list.(0) in
-    let portaddr = Unix.ADDR_INET (ipaddr, remote_port) in
-    lwt _ = Lwt_unix.sendto fd command 0 (String.length command) [] portaddr in
-    return ()
-  with _ -> 
-    usage ()
+  let server_ip = Config.external_ip in 
+  let remote_port = Config.signal_port in 
+  let tactic = 
+    try 
+      Sys.argv.(1) 
+    with _ -> 
+      usage ();
+      raise Exit
+  in
+              
+  try_lwt 
+    lwt fd = create_fd server_ip remote_port in
+    lwt src = try_lwt
+      let hent = Unix.gethostbyname server_ip in
+      return (Unix.ADDR_INET (hent.Unix.h_addr_list.(0), remote_port))
+    with _ ->
+      raise_lwt (Failure ("cannot resolve " ^ server_ip ))
+    in
+    lwt _ = Lwt_unix.connect fd src in
+    let rpc = Rpc.create_notification "exec_tactic" [tactic;"home";"slave"] in
+    let buf = Rpc.rpc_to_string rpc in
+    lwt _ = Lwt_unix.send fd buf 0 (String.length buf) [] in
+    let _ = Lwt_unix.shutdown fd Lwt_unix.SHUTDOWN_ALL in
+      return ()
+  with exn ->
+    return (printf "ERROR: %s\n%!" (Printexc.to_string exn))
